8f4ff7083cafb7479ada0a35f192bcd7e28b3920
dinatalee98 <156063220+dinatalee98@users.noreply.github.com>
2024-08-27 15:17:17 +0900

    Create Model.py

---
+++ Model.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def create_model():
+    model = models.Sequential([
+        layers.Dense(64, activation='relu', input_shape=(input_shape,)),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
