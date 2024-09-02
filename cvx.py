85bf260de2b14a2fe881ac36fddd4a7e3ff999f0
조신영(mac) <>
2024-09-02 13:50:42 +0900

    fix equations

--- cvx.py
+++ cvx.py
@@ -1,31 +1,36 @@
 import numpy as np
 import cvxpy as cp
 
+c = 3.8
+
 def euclidean_distance(uav_pos, device_pos, altitude): # System model d_k(n)
-    return np.sqrt(np.sum((uav_pos - device_pos) ** 2) + altitude ** 2)
+    return np.sqrt(np.sum((uav_pos - device_pos) ** 2) - altitude ** 2)
 
 def computation_time_energy(c_k, D_k, f_k, a_k, alpha_k): # Eq 3 & 4
     t_comp = (c_k * D_k) / f_k
     E_comp = a_k * (alpha_k / 2) * c_k * D_k * (f_k ** 2)
     return t_comp, E_comp
 
-def P_LoS(n, k):
+def P_LoS(n, k, H_u, d):
     zeta1 = 1
     zeta2 = 2
     theta_k = np.arctan(H_u/d[k][n]) # d[k][n] distance between UAV and IoT
     return 1 / (1 + zeta1 * np.exp(-zeta2 * (180 / np.pi * theta_k - zeta1)))
 
-def path_loss(d_k, fc, eta_LoS, eta_NLoS, P_LoS): # Eq 6 & 7 & 8
-    PL_LoS = 20 * np.log10((4 * np.pi * fc * d_k) / c) + eta_LoS
-    PL_NLoS = 20 * np.log10((4 * np.pi * fc * d_k) / c) + eta_NLoS
-    return P_LoS * PL_LoS + (1 - P_LoS) * PL_NLoS
+def PL_LoS(k, n, f_c, d, eta_LoS):
+    return 20 * np.log10((4 * np.pi * f_c * d[k][n]) / c) + eta_LoS
 
-# Eq 9, 10, 11 추가 필요
+def PL_NLoS(k, n, f_c, d, eta_NLoS):
+    return 20 * np.log10((4 * np.pi * f_c * d[k][n]) / c) + eta_NLoS
 
-def data_rate_and_energy(b_k, p_k, h_k, sigma2, a_k, s): # Eq 12, 13, 14
-    R_k = b_k * np.log2(1 + (p_k * np.linalg.norm(h_k) ** 2) / sigma2)
-    t_comm = (a_k * s) / R_k
-    E_comm = (t_comm * sigma2 * (2 ** (a_k * s / (b_k * t_comm)) - 1)) / np.linalg.norm(h_k) ** 2
+def path_loss(k, n, d, f_c, eta_LoS, eta_NLoS, P_LoS): # Eq 6 & 7 & 8
+    return P_LoS * PL_LoS(k, n, f_c, d, eta_LoS) + (1 - P_LoS) * PL_NLoS(k, n, f_c, d, eta_NLoS)
+
+
+def data_rate_and_energy(n, b_k, p_k, h_k, sigma, a_k, s): # Eq 12, 13, 14
+    R_k = b_k[n] * np.log2(1 + (p_k[n] * np.linalg.norm(h_k[n]) ** 2) / sigma ** 2)
+    t_comm = (a_k[n] * s) / R_k
+    E_comm = (t_comm * (sigma ** 2) * (2 ** (a_k[n] * s / (b_k[n] * t_comm)) - 1)) / np.linalg.norm(h_k[n]) ** 2
     return R_k, t_comm, E_comm
 
 
